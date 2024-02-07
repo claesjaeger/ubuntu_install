0301f35c993955bd747fd495cdf8daa9e4cfcfdb
Claes Jæger <claeslund@gmail.com>
2024-02-07 11:08:29 +0100

    Create installWSL.sh
    added new script

---
+++ installWSL.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# WSL install script
+
+
+sudo apt install -y git /
+                    git-flow /
+                    bash-completion /
+                    bash-builtins /
+                    powerline /
+                    powerline-gitstatus /
+
+git clone https://github.com/claesjaeger/bash_tools.git
+git clone https://github.com/claesjaeger/bashlib.git
