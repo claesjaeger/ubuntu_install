2a98e42a12039ec3f6a58893e118ca9f4b7cb5bb
Claes Jæger <claeslund@gmail.com>
2024-02-13 10:45:15 +0100

    Update installWSL.sh

--- installWSL.sh
+++ installWSL.sh
@@ -2,13 +2,29 @@
 
 # WSL install script
 
+# Fix: cannot create /var/lib/landscape/landscape-sysinfo.cache: Permission denied
+# https://askubuntu.com/questions/1414483/landscape-sysinfo-cache-permission-denied-when-i-start-ubuntu-22-04-in-wsl
+sudo apt remove -y landscape-common # Remove the package
+sudo apt autoremove -y              # Remove orphaned packages
+rm -rf ~/.landscape/                # Clean up, no longer used
 
-sudo apt install -y git /
-                    git-flow /
-                    bash-completion /
-                    bash-builtins /
-                    powerline /
-                    powerline-gitstatus /
+# Install packages
+sudo apt install -y git \
+                    git-flow \
+                    bash-completion \
+                    bash-builtins \
+                    powerline \
+                    powerline-gitstatus \
+                    dos2unix \
 
+# Get Bash environment
 git clone https://github.com/claesjaeger/bash_tools.git
 git clone https://github.com/claesjaeger/bashlib.git
+
+# Setup bash environment
+mv .bashrc org_bashrc
+mv .ssh org_ssh
+ln -s bash_tools/.bashrc .bashrc
+ln -s bash_tools/vim/.vimrc .vimrc
+ln -s bash_tools/ssh/SGRE .ssh
+ln -s ~/bash_tools/powerline .configpowerline
