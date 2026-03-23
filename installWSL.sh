240e6694157a9ec5a7fda2a2754ad5234eadb045
Claes Jæger <claeslund@gmail.com>
2026-03-23 16:20:13 +0100

    Update installWSL.sh

--- installWSL.sh
+++ installWSL.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
 # WSL install script
-
-# Fix: cannot create /var/lib/landscape/landscape-sysinfo.cache: Permission denied
-# https://askubuntu.com/questions/1414483/landscape-sysinfo-cache-permission-denied-when-i-start-ubuntu-22-04-in-wsl
-sudo apt remove -y landscape-common # Remove the package
-sudo apt autoremove -y              # Remove orphaned packages
-rm -rf ~/.landscape/                # Clean up, no longer used
+CURRENTDIR=$(pwd)
+cd ~
 
 # Update to latests and greatest
-sudo apt update
-sudo apt upgrade -y
+sudo apt-get update
+sudo apt-get upgrade -y
 
 # Install packages
-sudo apt install -y git \
+## graphviz is installed by plantUML
+## util-linux should have column, but apprently that is in bsdmainutils
+sudo apt-get install -y git \
                     git-flow \
                     bash-completion \
                     bash-builtins \
@@ -24,12 +22,20 @@ sudo apt install -y git \
                     dos2unix \
                     doxygen \
                     doxygen-gui \
-                    graphviz \
                     mscgen \
 
 # Get Bash environment
 git clone https://github.com/claesjaeger/bash_tools.git
 git clone https://github.com/claesjaeger/bashlib.git
+
+
+# PLANTUML
+PLANTUML_FILE="plantuml_install.sh"
+PLANTUML_SCRIPT="https://raw.githubusercontent.com/metanorma/plantuml-install/refs/heads/main/ubuntu.sh"
+curl -o $PLANTUML_FILE  -L $PLANTUML_SCRIPT
+chmod +x $PLANTUML_FILE
+bash $PLANTUML_FILE
+rm $PLANTUML_FILE
 
 # Setup bash environment
 mv .bashrc org_bashrc
@@ -38,3 +44,5 @@ ln -s bash_tools/.bashrc .bashrc
 ln -s bash_tools/vim/.vimrc .vimrc
 ln -s bash_tools/ssh/SGRE .ssh
 ln -s ~/bash_tools/powerline .config/powerline
+
+cd $CURRENTDIR
